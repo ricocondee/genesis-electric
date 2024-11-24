9d37d32edde7ec52aa9fd807aba9aa177898bec8
Emanuel Rico Conde <ricocondee@gmail.com>
2024-11-24 12:55:55 -0500

    [FIX] path
    Signed-off-by: Emanuel Rico Conde <ricocondee@gmail.com>

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../layout/Layout";
-import Home from "../pages/Home";
+import Home from "../Pages/Home";
 
 const App = () => {
   return (
