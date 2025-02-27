19010402c8e1ea0043b1b4e55745130129e4d3f6
Emanuel Rico Conde <ricocondee@gmail.com>
2025-02-26 21:23:01 -0500

    framer motion

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,9 +7,11 @@ import MyLink from "../components/MyLink";
 import CookieCons from "../components/CookieConsent"
 import { TbBrandWhatsapp } from "react-icons/tb";
 import Reviews from "../components/Reviews";
+import {motion} from "framer-motion";
 
 const Home = () => {
   return (
+    <motion.div initial={{opacity: 0}} animate={{opacity: 1}} exit={{opacity: 0}} transition={{duration: 1.5}}>
     <article id="home">
       <section className={HomeStyles.container}>
         <div className={HomeStyles.text__container}>
@@ -35,6 +37,7 @@ const Home = () => {
       <Contact />
       <CookieCons/>
     </article>
+    </motion.div>
   );
 };
 
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,13 +1,16 @@
 import Items from '../containers/Items'
 import ProductStyles from '../styles/Product.module.css'
+import {motion} from "framer-motion";
 
 const Products = () => {
   return (
+    <motion.div initial={{opacity: 0}} animate={{opacity: 2}} exit={{opacity: 0}} transition={{duration: 1.5}}>
     <div className={ProductStyles.container}>
       <h1 alt="Aires acondicionados baratos">¡Aires al mejor precio!</h1>
         <Items />
         <strong>*Todos los precios son con IVA incluido. Para aires centrales o de mayor capacidad cont&aacute;ctanos a nuestro WhatsApp o nuestro correo electr&oacute;nico</strong>
     </div>
+    </motion.div>
   )
 }
 
