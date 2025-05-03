d103046af66bda5268a7f8aa8c4d3d45fe2390a3
Emanuel Rico Conde <ricocondee@gmail.com>
2025-05-02 21:42:15 -0500

    pwa
    Signed-off-by: Emanuel Rico Conde <ricocondee@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,58 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate', // Auto-update the service worker when the app is opened
+      manifest: {
+        name: 'Genesis Electric',
+        short_name: 'Genesis',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#0a2472',
+        theme_color: '#0a2472',
+        icons: [
+          {
+            src: '/assets/icons/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/assets/icons/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.(googleapis|gstatic)\.com\/.*/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'google-fonts',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365 // Cache for 1 year
+              }
+            }
+          },
+          {
+            urlPattern: /^https:\/\/.*\.png$/,
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 20,
+                maxAgeSeconds: 60 * 60 * 24 * 30 // Cache for 30 days
+              }
+            }
+          }
+        ]
+      }
+    })
+  ]
+});
