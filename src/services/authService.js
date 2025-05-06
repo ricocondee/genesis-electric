d3bcac6029745e8ac9f60eaa177ddf26f063f13a
Emanuel Rico Conde <43344906+ricocondee@users.noreply.github.com>
2025-05-06 13:20:13 -0400

    api url

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL + "/api/auth";
+const API_URL = "https://api.genesiselectricsas.com/api/auth";
 
 // Registro de usuario
 export const register = async (userData, token) => {
